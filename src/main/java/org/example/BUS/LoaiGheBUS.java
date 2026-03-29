ce9af35161ee3fe5923d6ad781e2e3812d7a36e1
aaaaaa <9bi123@gmail.com>
2026-03-29 21:39:25 +0700

    Update: LoaiGheBUS

---
+++ src/main/java/org/example/BUS/LoaiGheBUS.java
@@ -0,0 +1,13 @@
+package org.example.BUS;
+
+import org.example.DAO.LoaiGheDAO;
+import org.example.DTO.LoaiGheDTO;
+import java.util.ArrayList;
+
+public class LoaiGheBUS {
+    private final LoaiGheDAO loaiGheDAO = new LoaiGheDAO();
+
+    public ArrayList<LoaiGheDTO> getList() {
+        return loaiGheDAO.selectAll();
+    }
+}
