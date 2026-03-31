75183692e4d433dbeded2a383869eb68eabd34bf
AN HOANG <kazutohoang89@gmail.com>
2026-03-31 14:26:51 +0700

    Test

--- src/main/java/org/example/BUS/LoaiGheBUS.java
+++ src/main/java/org/example/BUS/LoaiGheBUS.java
@@ -2,7 +2,7 @@ package org.example.BUS;
 
 import org.example.DAO.LoaiGheDAO;
 import org.example.DTO.LoaiGheDTO;
-<<<<<<< HEAD
+
 import java.util.ArrayList;
 
 public class LoaiGheBUS {
@@ -12,29 +12,3 @@ public class LoaiGheBUS {
         return loaiGheDAO.selectAll();
     }
 }
-=======
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class LoaiGheBUS {
-
-    private final LoaiGheDAO loaiGheDAO = new LoaiGheDAO();
-    private List<LoaiGheDTO> listLoaiGhe = new ArrayList<>();
-
-    public LoaiGheBUS() {
-        refreshList();
-    }
-
-    public void refreshList() {
-        listLoaiGhe = loaiGheDAO.selectAll();
-    }
-
-    public List<LoaiGheDTO> getList() {
-        if (listLoaiGhe == null || listLoaiGhe.isEmpty()) {
-            refreshList();
-        }
-        return listLoaiGhe;
-    }
-}
->>>>>>> 98db611 (feat:  Three layer TheLoaiPhim add UI)
